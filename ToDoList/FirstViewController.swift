e98a7b2d03d3d8169735f94ff644ab4535148b24
Sachin Virdy <svirdy1@gmail.com>
2015-10-25 15:18:45 -0700

    first layout

--- ToDoList/FirstViewController.swift
+++ ToDoList/FirstViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var toDoList = [String]()
 
+class FirstViewController: UIViewController, UITableViewDelegate {
+
+    @IBOutlet var toDoListTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,17 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoList.count
+    }
+    
+
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = toDoList[indexPath.row]
+        return cell
+    }
 
 }
 
